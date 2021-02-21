02cca6c0cf3a2d7298eccc861261e32bd1b90855
sina azar <s_azarfarshi@outlook.com>
2021-02-21 18:15:27 -0500

    third commit

--- TaghvimLib/src/main/java/com/sina/taghvimlib/MainActivity.kt
+++ TaghvimLib/src/main/java/com/sina/taghvimlib/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.sina.taghvimlib
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import net.time4j.PlainDate
 import net.time4j.SystemClock
 import net.time4j.calendar.PersianCalendar
 
-public class TaghvimFarsi {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getToday()
+    }
     companion object{
         public fun getToday(): PersianCalendar {
             val today: PlainDate = SystemClock.inLocalView().today()
             return today.transform(PersianCalendar::class.java)
         }
     }
-
 }
